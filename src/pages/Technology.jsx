0fa2c04e8eb6b91fd18740a8847f5080f0859b61
Jason McBride <jasonmcbride@engineeringtoeden.com>
2026-03-06 04:39:28 +0000

    File changes

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -386,7 +386,7 @@ export default function Technology() {
                     className="w-full h-full"
                     imgClassName="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
                   />
-                  <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent" />
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent pointer-events-none" />
                 </div>
                 <div className="flex-1 p-6 flex flex-col">
                   <div className="mb-4">
