3fb1dcf75c493b5e259fd0c9fd54957ad9268c2d
Jason McBride <jasonmcbride@engineeringtoeden.com>
2026-03-07 00:39:22 +0000

    File changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -419,7 +419,7 @@ export default function Projects() {
               </table>
             </div>
           )
-        )}
+        ))}
       </div>
       {(showAddModal || editingProject) && (
         <ProjectEditModal
