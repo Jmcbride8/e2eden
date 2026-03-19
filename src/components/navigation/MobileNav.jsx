6c83d8105e0a3faf25db467379302cfc8020e47d
Jason McBride <jasonmcbride@engineeringtoeden.com>
2026-03-19 18:57:34 +0000

    File changes

--- src/components/navigation/MobileNav.jsx
+++ src/components/navigation/MobileNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Home, Lightbulb, Map, DollarSign, Handshake } from "lucide-react";
+import { Home, Lightbulb, Map, Handshake } from "lucide-react";
 import { createPageUrl } from "../../utils";
 
 export default function MobileNav() {
