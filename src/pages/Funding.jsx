03d04772974f6490b210cabb53aa4aad2d7b5bae
Jason McBride <jmcbride.isye@gmail.com>
2026-04-22 07:17:59 +0000

    File changes

--- src/pages/Funding.jsx
+++ src/pages/Funding.jsx
@@ -46,7 +46,7 @@ function CapitalRaiseCard({ delay, image, tag, amount, subtitle, description, ch
           alt={tag}
           className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
         />
-        <div className="absolute inset-0 bg-gradient-to-r from-black/95 via-black/80 to-black/30" />
+        <div className="absolute inset-0 bg-gradient-to-r from-black/95 via-black/60 to-transparent" />
       </div>
 
       {/* Content */}
